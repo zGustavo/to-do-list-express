a09599422f23fa78c436ccd2bc7d8b5ba8c93b54
Gustavo Dias <dev.gustavo@hotmail.com>
2022-05-27 15:01:12 -0300

    Fim lista de TODO

--- src/routes/task.js
+++ src/routes/task.js
@@ -45,6 +45,17 @@ checklistDepedentRoute.post('/:id/tasks', async (req, res) => {
     }
 })
 
+simpleRouter.put('/:id', async (req, res) => {
+    let task = await Task.findById(req.params.id);
+    try {
+        task.set(req.body.task);
+        await task.save();
+        res.status(200).json({ task });
+    } catch (error) {
+        let errors = error.errors;
+        res.status(422).json({ task: {...errors}});
+    }
+})
 
 module.exports = {
      checklistDepedent: checklistDepedentRoute,
